63e0dd2f0ff42756c02cfa53802f873b8a9b4f87
tanvirsuperanova <tanvir@supernovatechbd.com>
2024-03-19 15:47:40 +0600

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 // import Image from "next/image";
 
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <div>Hello Weather App</div>;
+  return (
+    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+      <Navbar />
+      Hello Weather App
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { MdWbSunny, MdMyLocation, MdOutlineLocationOn } from "react-icons/md";
+import SearchBox from "./SearchBox";
 
 type Props = {};
 
 const Navbar = ({}: Props) => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-20 container mx-auto px-4 w-full flex justify-between items-center">
+        <p className="flex justify-center items-center gap-2">
+          <h2 className="text-grey-500 text-3xl">Weather</h2>
+          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
+        </p>
+        <div className="flex gap-2 items-center">
+          <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdOutlineLocationOn className="text-3xl" />
+          <p className="text-slate-900/80 text-sm">Bangladesh</p>
+          <SearchBox value={value}/>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
